3074264a96bc847fa2accb7a736eaa7e434a9b0b
jakeparcell14 <jakeparcell14@gmail.com>
2018-05-14 17:04:07 -0700

    added class description to custom control skin

--- src/ChessCustomControlSkin.java
+++ src/ChessCustomControlSkin.java
@@ -1,15 +1,14 @@
-/*
- * This class references the project at this link: https://github.com/GuiBon/ChessGame
- */
 
-
-//basic implementation of a Skin
 
 //imports 
 import javafx.scene.control.Skin;
 import javafx.scene.control.SkinBase;
 
-//class definition 
+/*
+ * This class references the project at this link: https://github.com/GuiBon/ChessGame
+ * 
+ * This class is a basic implementation of a Skin and assists with the formatting of the chess game
+ */
 class ChessCustomControlSkin extends SkinBase<ChessCustomControl> implements Skin<ChessCustomControl> {
 	public ChessCustomControlSkin(ChessCustomControl cc) {
 		//call the super class constructor
