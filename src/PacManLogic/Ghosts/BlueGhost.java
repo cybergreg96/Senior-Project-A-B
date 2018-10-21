6ab0bc6aa0c9797a65e71979d63e9a5726f5d207
Greg Dailey <greg.calway@gmail.com>
2018-10-21 10:55:12 -0700

    player 2 can switch between all ghosts. Upon being eaten player 2 auto switches and switches to new ghost

--- src/PacManLogic/Ghosts/BlueGhost.java
+++ src/PacManLogic/Ghosts/BlueGhost.java
@@ -15,6 +15,8 @@ import com.sun.org.apache.xml.internal.security.keys.content.KeyValue;
 public class BlueGhost extends Ghosts implements GameObject {
 	boolean human = true;
 	boolean comp = false;
+	boolean dead = false;
+	boolean caneat = false;
 	public int count = 0;
 
 	public BlueGhost(Point position, MapOutline map, Player player, Biscuits biscuits) {
@@ -25,19 +27,16 @@ public class BlueGhost extends Ghosts implements GameObject {
 	@Override
 	public void update(KeyCode keyCode) {
 		if (keyCode.equals(keyCode.ENTER)) {
-			count++;
-			System.out.println(count);
-			if(count==2){
-				count=0;
+			switchGhost();
+			if (getGhost() == "blue") {
+				human = true;
+				comp = false;
+			} else if (getGhost() != "blue") {
+				comp = true;
+				human = false;
 			}
 		}
-		if (count % 2 == 0) {
-			human = true;
-			comp = false;
-		} else if (count % 2 == 1) {
-			comp = true;
-			human = false;
-		}
+
 		if (getEscapeTimeCount() > 0) {
 			setEscape(true);
 			setEscapeTimeCount(getEscapeTimeCount() - 1);
@@ -50,15 +49,15 @@ public class BlueGhost extends Ghosts implements GameObject {
 		}
 
 		if (isEscape()) {
-			if ((keyCode == KeyCode.RIGHT) && (getPlayer().getX() == getX() - 20) && (getPlayer().getY() == getY())) {
+			if ((keyCode == KeyCode.D) && (getPlayer().getX() == getX() - 20) && (getPlayer().getY() == getY())) {
 				isEatenWhileEscape = true;
-			} else if ((keyCode == KeyCode.DOWN) && (getPlayer().getX() == getX())
+			} else if ((keyCode == KeyCode.S) && (getPlayer().getX() == getX())
 					&& (getPlayer().getY() == getY() - 20)) {
 				isEatenWhileEscape = true;
-			} else if ((keyCode == KeyCode.LEFT) && (getPlayer().getX() == getX() + 20)
+			} else if ((keyCode == KeyCode.A) && (getPlayer().getX() == getX() + 20)
 					&& (getPlayer().getY() == getY())) {
 				isEatenWhileEscape = true;
-			} else if ((keyCode == KeyCode.RIGHT) && (getPlayer().getX() == getX())
+			} else if ((keyCode == KeyCode.W) && (getPlayer().getX() == getX())
 					&& (getPlayer().getY() == getY() + 20)) {
 				isEatenWhileEscape = true;
 			} else if ((getPlayer().getX() == getX()) && (getPlayer().getY() == getY())) {
@@ -73,11 +72,11 @@ public class BlueGhost extends Ghosts implements GameObject {
 				bestFirstSearch(getPlayer().getStepX(), getPlayer().getStepY());
 
 			} else if (isEatenWhileEscape) {
-
 				bestFirstSearch(16, 15);
 				if ((getStepX() == 16) && (getStepY() == 15))
 					isEatenWhileEscape = false;
 			}
+
 		} else if (human == true && comp == false) {
 			switch (keyCode) {
 			case DOWN:
@@ -103,16 +102,34 @@ public class BlueGhost extends Ghosts implements GameObject {
 
 	}
 
+	public boolean isDead() {
+		return dead;
+
+	}
+	public boolean eat() {
+		return caneat;
+
+	}
+
 	@Override
 	public void draw(GraphicsContext g, SceneInfo sceneInfo) {
 		if (isEatenWhileEscape) {
-
 			g.setFill(javafx.scene.paint.Color.rgb(170, 170, 170));
-
+			dead = true;
+			if (getGhost() == "blue") {
+				human = true;
+				comp = false;
+			} else if (getGhost() != "blue") {
+				comp = true;
+				human = false;
+			}
 		} else if (!isEscape()) {
 			g.setFill(javafx.scene.paint.Color.rgb(0, 30, 200));
+			dead = false;
+			caneat = false;
 		} else {
 			g.setFill(javafx.scene.paint.Color.rgb(0, 155, 194));
+			caneat = true;
 		}
 		g.fillRoundRect(this.getX(), this.getY(), 20, 20, 2, 2);
 
--- src/PacmanController.java
+++ src/PacmanController.java
@@ -1,3 +1,4 @@
+
 /*
  * This class references the project at this link: https://github.com/arashabedin/PacMan
  *
@@ -48,197 +49,239 @@ public class PacmanController {
 	private ToolBar birdstufzBanner1;
 	@FXML
 	private StackPane stackpane;
-	
+
 	@FXML
-    Label labelStatus;
-    @FXML
-    Canvas root;
+	Label labelStatus;
+	@FXML
+	Canvas root;
 
-    @FXML
-    Label myPoints = new Label();
+	@FXML
+	Label myPoints = new Label();
 
-    @FXML
-    Button restartBtn = new Button();
+	@FXML
+	Button restartBtn = new Button();
 
-    @FXML
-    Label blueGhostSearches = new Label();
+	@FXML
+	Label blueGhostSearches = new Label();
 
-    @FXML
-    Label pinkGhostSearches = new Label();
+	@FXML
+	Label pinkGhostSearches = new Label();
 
-    @FXML
-    Label orangeGhostSearches = new Label();
+	@FXML
+	Label orangeGhostSearches = new Label();
 
-    @FXML
-    Label redGhostSearches = new Label();
+	@FXML
+	Label redGhostSearches = new Label();
 
+	private SceneInfo sceneInfo;
+	private float refreshRate = 50;
+	private KeyCode keyPressed = KeyCode.ALT;
+	private KeyCode ghostKeyPressed = KeyCode.ALT;
 
+	ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
+	private Player player;
 
-    private SceneInfo sceneInfo;
-    private float refreshRate = 50;
-    private KeyCode keyPressed = KeyCode.ALT;
-    private KeyCode ghostKeyPressed = KeyCode.ALT;
+	private MapOutline map = new MapOutline();
+	private Biscuits biscuits;
+	private PinkGhost pinkGhost;
+	private BlueGhost blueGhost;
+	private OrangeGhost orangeGhost;
+	private RedGhost redGhost;
+	private int bcount = 0;
+	private int rcount = 0;
+	private int ocount = 0;
+	private int pcount = 0;
+	private NonPlayerCharacter npc;
 
-    ArrayList < GameObject > gameObjects = new ArrayList < GameObject > ();
-    private Player player;
+	MazePlayGround mazePlayGround = new MazePlayGround(javafx.scene.paint.Color.rgb(0, 0, 0), 1, 1, player, map);
 
-    private MapOutline map = new MapOutline();
-    private Biscuits biscuits;
-    private PinkGhost pinkGhost;
-    private BlueGhost blueGhost;
-    private OrangeGhost orangeGhost;
-    private RedGhost redGhost;
-    private NonPlayerCharacter npc;
+	public void addStuff() {
 
+		sceneInfo = new SceneInfo(root);
+		player = new Player(new Point(20, 60), map);
 
-    MazePlayGround mazePlayGround = new MazePlayGround(javafx.scene.paint.Color.rgb(0, 0, 0), 1, 1, player, map);
+		biscuits = new Biscuits(player, map);
+		biscuits.setTotalEatenBiscuits(0);
+		biscuits.setTotalEatenBigBiscuits(0);
+		pinkGhost = new PinkGhost(new Point(280, 300), map, player, biscuits);
+		blueGhost = new BlueGhost(new Point(220, 300), map, player, biscuits);
+		orangeGhost = new OrangeGhost(new Point(260, 260), map, player, biscuits);
+		redGhost = new RedGhost(new Point(260, 300), map, player, biscuits);
 
-    public void addStuff() {
+		// NPC initializer
+		npc = new NonPlayerCharacter(player, map);
 
-        sceneInfo = new SceneInfo(root);
-        player = new Player(new Point(20, 60), map);
+		gameObjects.add(biscuits);
+		gameObjects.add(player);
+		// gameObjects.add(pinkGhost);
+		// gameObjects.add(blueGhost); don't add blue ghost (player 2) to
+		// separate its update and drawing
+		// gameObjects.add(orangeGhost);
+		// gameObjects.add(redGhost);
+		gameObjects.add(mazePlayGround);
+		gameObjects.add(npc);
 
-        biscuits = new Biscuits(player, map);
-        biscuits.setTotalEatenBiscuits(0);
-        biscuits.setTotalEatenBigBiscuits(0);
-        pinkGhost = new PinkGhost(new Point(280, 300), map, player, biscuits);
-        blueGhost = new BlueGhost(new Point(220, 300), map, player, biscuits);
-        orangeGhost = new OrangeGhost(new Point(260, 260), map, player, biscuits);
-        redGhost = new RedGhost(new Point(260, 300), map, player, biscuits);
-        
-        // NPC initializer
-        npc = new NonPlayerCharacter(player, map);
-        
-        gameObjects.add(biscuits);
-        gameObjects.add(player);
-        gameObjects.add(pinkGhost);
-        //gameObjects.add(blueGhost); don't add blue ghost (player 2) to separate its update and drawing
-        gameObjects.add(orangeGhost);
-        gameObjects.add(redGhost);
-        gameObjects.add(mazePlayGround);
-        gameObjects.add(npc);
+	}
 
-    }
-    public void restartGame() {
-        pinkGhost.setEscapeTimeCount(0);
-        pinkGhost.setEscape(false);
-        gameObjects = new ArrayList < GameObject > ();
-        keyPressed = KeyCode.ALT;
-        ghostKeyPressed = KeyCode.ALT;
+	public void restartGame() {
+		pinkGhost.setEscapeTimeCount(0);
+		pinkGhost.setEscape(false);
+		gameObjects = new ArrayList<GameObject>();
+		keyPressed = KeyCode.ALT;
+		ghostKeyPressed = KeyCode.ALT;
 
+		addStuff();
 
-        addStuff();
+	}
 
-    }
-    public void initialize() {
-        restartBtn.setOnAction(new EventHandler < ActionEvent > () {
-            @Override
-            public void handle(ActionEvent event) {
-                restartGame();
+	public void initialize() {
+		restartBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				restartGame();
 
-            }
-        });
+			}
+		});
 
+		addStuff();
 
+		Button goHome = new Button("Go home");
+		Scene home = new Scene(goHome);
+		home.snapshot(null);
+		goHome.setTranslateX(4);
+		goHome.setTranslateY((650 / 2) - (goHome.getHeight() / 2) - 50);
+		goHome.setRotate(-90);
+		rootPane.getChildren().add(goHome);
 
-        addStuff();
-        
-        Button goHome = new Button("Go home");
-	    Scene home = new Scene(goHome);
-	    home.snapshot(null);       
-	    goHome.setTranslateX(4);
-	    goHome.setTranslateY((650 / 2) - (goHome.getHeight() / 2)  - 50); 
-	    goHome.setRotate(-90);
-	    rootPane.getChildren().add(goHome);
-	    
-	    //handles the "Go Home" button
-	    goHome.setOnAction((ActionEvent e) -> {
-	    	try 
-	    	{
+		// handles the "Go Home" button
+		goHome.setOnAction((ActionEvent e) -> {
+			try {
 				Parent x = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
 				x.setStyle("-fx-background-color: #a50000");
-	            Scene y = new Scene(x);
-	            Stage w = (Stage)((Node)e.getSource()).getScene().getWindow();
-	            w.setResizable(false);
-	            w.setScene(y);
-			} 
-	    	catch (IOException e1) 
-	    	{
+				Scene y = new Scene(x);
+				Stage w = (Stage) ((Node) e.getSource()).getScene().getWindow();
+				w.setResizable(false);
+				w.setScene(y);
+			} catch (IOException e1) {
 				e1.printStackTrace();
 			}
-	    });
+		});
 
-        new AnimationTimer() {
-            long lastUpdate;
-            public void handle(long now) {
-                if (now > lastUpdate + refreshRate * 1690000) {
-                    lastUpdate = now;
-                    update(now);
-                }
-            }
-        }.start();
+		new AnimationTimer() {
+			long lastUpdate;
 
-    }
+			public void handle(long now) {
+				if (now > lastUpdate + refreshRate * 1690000) {
+					lastUpdate = now;
+					update(now);
+				}
+			}
+		}.start();
 
+	}
 
-    //set different keyPressed variables based on what type of key was pressed, WASD (letter key) for P1 and arrow keys for P2
-    public void keyPressed(KeyCode keyCode) {
-    	if(keyCode.isLetterKey()) {
-    		this.keyPressed = keyCode;
-    	}
-        if(keyCode.isArrowKey()) {
-        	this.ghostKeyPressed = keyCode;
-        }
-        if(keyCode == keyCode.ENTER) {
-        	this.ghostKeyPressed = keyCode.ENTER;
-        }
-    }
-    
+	// set different keyPressed variables based on what type of key was pressed,
+	// WASD (letter key) for P1 and arrow keys for P2
+	public void keyPressed(KeyCode keyCode) {
+		if (keyCode.isLetterKey()) {
+			this.keyPressed = keyCode;
+		}
+		if (keyCode.isArrowKey()) {
+			this.ghostKeyPressed = keyCode;
+		}
+		if (keyCode == keyCode.ENTER) {
+			this.ghostKeyPressed = keyCode.ENTER;
+		}
+	}
 
-    private void update(long now) {
+	private void update(long now) {
 
-    	//separated the 2nd player ghost being updated with a different keyCode to fix problems with one player not being able to move when the other presses a different key
-    	blueGhost.update(ghostKeyPressed);
-    	
-        for (int i = 0; i < gameObjects.size(); i++) {
+		// separated the 2nd player ghost being updated with a different keyCode
+		// to fix problems with one player not being able to move when the other
+		// presses a different key
 
-            gameObjects.get(i).update(keyPressed);
-            
+		if (blueGhost.getGhost() == "blue" && blueGhost.isDead() && bcount == 0) {
+			bcount++;
+			blueGhost.switchGhost();
+			this.ghostKeyPressed = ghostKeyPressed.ENTER;
+		} else if (!blueGhost.isDead() && bcount != 0 && redGhost.getGhost() == "red") {
+			bcount = 0;
+		}
+		if (redGhost.getGhost() == "red" && redGhost.isDead() && rcount == 0) {
+			rcount++;
+			redGhost.switchGhost();
+			this.ghostKeyPressed = ghostKeyPressed.ENTER;
+		} else if (!redGhost.isDead() && rcount != 0 && orangeGhost.getGhost() == "orange") {
+			rcount = 0;
+		}
+		if (orangeGhost.getGhost() == "orange" && orangeGhost.isDead() && ocount == 0) {
+			ocount++;
+			orangeGhost.switchGhost();
+			this.ghostKeyPressed = ghostKeyPressed.ENTER;
+		} else if (!orangeGhost.isDead() && ocount != 0 && pinkGhost.getGhost() == "pink") {
+			ocount = 0;
+		}
+		if (pinkGhost.getGhost() == "pink" && pinkGhost.isDead() && pcount == 0) {
+			pcount++;
+			pinkGhost.switchGhost();
+			this.ghostKeyPressed = ghostKeyPressed.ENTER;
+		} else if (!pinkGhost.isDead() && pcount != 0 && blueGhost.getGhost() == "blue") {
+			pcount = 0;
+		}
+		blueGhost.update(ghostKeyPressed);
+		redGhost.update(ghostKeyPressed);
+		orangeGhost.update(ghostKeyPressed);
+		pinkGhost.update(ghostKeyPressed);
 
-        }
+		if (this.ghostKeyPressed == ghostKeyPressed.ENTER) {
+			this.ghostKeyPressed = ghostKeyPressed.C;
+		} else if (this.ghostKeyPressed != ghostKeyPressed.ENTER) {
+			this.ghostKeyPressed = this.ghostKeyPressed;
+		}
+		for (int i = 0; i < gameObjects.size(); i++) {
 
-        // keyPressed = KeyCode.ALT;
-        if (pinkGhost.isEscape() == false)
-            if (((player.getX() == pinkGhost.getX()) && (player.getY() == pinkGhost.getY())) || ((player.getX() == blueGhost.getX()) && (player.getY() == blueGhost.getY())) || ((player.getX() == orangeGhost.getX()) && (player.getY() == orangeGhost.getY())) || ((player.getX() == redGhost.getX()) && (player.getY() == redGhost.getY())))
+			gameObjects.get(i).update(keyPressed);
 
-            {
-                System.out.println("You lost");
-                restartGame();
-            }
-        drawCanvas();
-        myPoints.setText("Points : " + String.valueOf((biscuits.getTotalEatenBiscuits() * 100) - 100));
-        pinkGhostSearches.setText((pinkGhost.getTotalSearched() > 0 ? String.valueOf(pinkGhost.getTotalSearched()) : "Random mode"));
-        orangeGhostSearches.setText((orangeGhost.getTotalSearched() > 0 ? String.valueOf(orangeGhost.getTotalSearched()) : "Random mode"));
-        redGhostSearches.setText((redGhost.getTotalSearched() > 0 ? String.valueOf(redGhost.getTotalSearched()) : "Random mode"));
-    }
+		}
+		// if (pinkGhost.isEscape() == false)
+		if ((!pinkGhost.eat()&&(player.getX() == pinkGhost.getX()) && (player.getY() == pinkGhost.getY()))
+				|| (!blueGhost.eat() && (player.getX() == blueGhost.getX()) && (player.getY() == blueGhost.getY()))
+				|| (!orangeGhost.eat() && (player.getX() == orangeGhost.getX())
+						&& (player.getY() == orangeGhost.getY()))
+				|| (!redGhost.eat() && (player.getX() == redGhost.getX()) && (player.getY() == redGhost.getY()))) {
+			System.out.println("You lost");
+			restartGame();
+		}
+		drawCanvas();
+		myPoints.setText("Points : " + String.valueOf((biscuits.getTotalEatenBiscuits() * 100) - 100));
+		pinkGhostSearches.setText(
+				(pinkGhost.getTotalSearched() > 0 ? String.valueOf(pinkGhost.getTotalSearched()) : "Random mode"));
+		orangeGhostSearches.setText(
+				(orangeGhost.getTotalSearched() > 0 ? String.valueOf(orangeGhost.getTotalSearched()) : "Random mode"));
+		redGhostSearches.setText(
+				(redGhost.getTotalSearched() > 0 ? String.valueOf(redGhost.getTotalSearched()) : "Random mode"));
+	}
 
+	/**
+	 * Draw the canvas - used in the gameloop
+	 */
+	private void drawCanvas() {
+		GraphicsContext g = root.getGraphicsContext2D();
 
-    /**
-     * Draw the canvas - used in the gameloop
-     */
-    private void drawCanvas() {
-        GraphicsContext g = root.getGraphicsContext2D();
+		// clear canvas
+		g.clearRect(0, 0, sceneInfo.getWidth() * sceneInfo.getFieldWidth(),
+				sceneInfo.getHeight() * sceneInfo.getFieldHeight());
 
-        // clear canvas
-        g.clearRect(0, 0, sceneInfo.getWidth() * sceneInfo.getFieldWidth(), sceneInfo.getHeight() * sceneInfo.getFieldHeight());
+		// draw gameObjects
+		for (GameObject item : gameObjects) {
+			item.draw(g, sceneInfo);
+		}
+		blueGhost.draw(g, sceneInfo);
+		redGhost.draw(g, sceneInfo);
+		orangeGhost.draw(g, sceneInfo);
+		pinkGhost.draw(g, sceneInfo);
 
-        // draw gameObjects
-        for (GameObject item: gameObjects) {
-            item.draw(g, sceneInfo);
-        }
-        blueGhost.draw(g, sceneInfo);
-
-        // biscuits.clear(g,sceneInfo);
-    }
+		// biscuits.clear(g,sceneInfo);
+	}
 
 }
